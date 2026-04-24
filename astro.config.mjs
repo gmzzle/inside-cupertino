15a089b0c1ff451f8c0872b3ac44200ce1c2c1ac
Greg <greg@Gregs-MacBook-Air.local>
2026-04-24 00:30:04 -0700

    Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://insidecupertino.com',
-  integrations: [tailwind({ applyBaseStyles: false })],
+  integrations: [
+    tailwind({ applyBaseStyles: false }),
+    sitemap({
+      filter: (page) => !page.includes('/draft'),
+    }),
+  ],
   build: { inlineStylesheets: 'auto' },
 });
